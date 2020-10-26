4914a5a4b042b5c79e1d6e9544e8286cf868b74f
Jamie Everett <jreverett2442@gmail.com>
2020-10-26 15:27:02 +0000

    Create category repository interface and implement it

---
+++ BulkyBook.DataAccess/Repository/CategoryRepository.cs
@@ -0,0 +1,32 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CategoryRepository : Repository<Category>, ICategoryRepository
+    {
+        private readonly ApplicationDbContext db;
+
+        public CategoryRepository(ApplicationDbContext db) : base(db)
+        {
+            this.db = db;
+        }
+
+        public void Update(Category category)
+        {
+            var categoryFromDb = db.Categories.FirstOrDefault(x => x.Id == category.Id);
+
+            if (categoryFromDb != null)
+            {
+                categoryFromDb.Name = category.Name;
+                db.SaveChanges();
+            }
+        }
+    }
+}
