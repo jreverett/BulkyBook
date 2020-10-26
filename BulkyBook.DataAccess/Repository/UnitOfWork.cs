673237de2f03b5ae8d7c2b770affe2b0ac2f586a
Jamie Everett <jreverett2442@gmail.com>
2020-10-26 16:25:40 +0000

    Create and implement unit of work interface, register it as a startup.cs service

---
+++ BulkyBook.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,33 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ApplicationDbContext db;
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            this.db = db;
+            Category = new CategoryRepository(this.db);
+            SP_Call = new SP_Call(this.db);
+        }
+
+        public ICategoryRepository Category { get; private set; }
+        public ISP_Call SP_Call { get; private set; }
+
+        public void Dispose()
+        {
+            db.Dispose();
+        }
+
+        public void Save()
+        {
+            db.SaveChanges();
+        }
+    }
+}
