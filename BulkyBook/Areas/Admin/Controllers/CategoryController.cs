fc9ec5ae35f4d4af900b3b723c3260d8f8c67c50
Jamie Everett <jreverett2442@gmail.com>
2020-10-27 12:09:05 +0000

    Add category controller with GetAll api call. Add view to menu dropdown.

---
+++ BulkyBook/Areas/Admin/Controllers/CategoryController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BulkyBook.DataAccess.Repository.IRepository;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class CategoryController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+
+        public CategoryController(IUnitOfWork unitOfWork)
+        {
+            this.unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var categories = unitOfWork.Category.GetAll();
+            return Json(new { data = categories });
+        }
+
+        #endregion
+    }
+}
