158cc68d974460ce8e595036658b87e7604334ea
Jamie Everett <jreverett2442@gmail.com>
2020-10-30 17:06:12 +0000

    Add ApplicationUser entity

---
+++ BulkyBook.DataAccess/Migrations/20201030154923_AddUserPropertiesToDb.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BulkyBook.DataAccess.Migrations
+{
+    public partial class AddUserPropertiesToDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Town",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "County",
+                table: "AspNetUsers",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "County",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Town",
+                table: "AspNetUsers",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+    }
+}
---
+++ BulkyBook.Models/ApplicationUser.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BulkyBook.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string County { get; set; }
+
+        public string Postcode { get; set; }
+
+        public int? CompanyId { get; set; }
+
+        [ForeignKey("CompanyId")]
+        public Company Company { get; set; }
+
+        [NotMapped]
+        public string Role { get; set; }
+    }
+}
