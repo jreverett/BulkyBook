bfedf1832a312c6df3a9c2024921e4398f961975
Jamie Everett <jreverett2442@gmail.com>
2020-11-03 12:21:38 +0000

    Add role string constants

--- BulkyBook.Utility/SD.cs
+++ BulkyBook.Utility/SD.cs
@@ -7,5 +7,10 @@
         public const string Proc_CoverType_GetAll = "usp_GetCoverTypes";
         public const string Proc_CoverType_Update = "usp_UpdateCoverType";
         public const string Proc_CoverType_Delete = "usp_DeleteCoverType";
+
+        public const string Role_Admin = "Admin";
+        public const string Role_Employee = "Employee";
+        public const string Role_User_Individual = "Individual Customer";
+        public const string Role_User_Company = "Company Customer";
     }
 }
