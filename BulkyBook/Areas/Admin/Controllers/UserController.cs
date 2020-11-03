c99c0f5a81a3c06ed1daf90c5e615b683d98b68c
Jamie Everett <jreverett2442@gmail.com>
2020-11-03 15:21:42 +0000

    Add UserController and View (GetAll)

---
+++ BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,51 @@
+﻿using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace BulkyBook.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class UserController : Controller
+    {
+        private readonly IUnitOfWork unitOfWork;
+        private readonly UserManager<IdentityUser> userManager;
+
+        public UserController(IUnitOfWork unitOfWork, UserManager<IdentityUser> userManager)
+        {
+            this.unitOfWork = unitOfWork;
+            this.userManager = userManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = unitOfWork.ApplicationUser.GetAll(null, null, "Company");
+
+            foreach (var user in users)
+            {
+                user.Role = userManager.GetRolesAsync(user).Result.FirstOrDefault();
+
+                if (user.Company == null)
+                {
+                    user.Company = new Company()
+                    {
+                        Name = ""
+                    };
+                }
+            }
+
+            return Json(new { data = users });
+        }
+
+        #endregion
+    }
+}
---
+++ BulkyBook/wwwroot/js/User.js
@@ -0,0 +1,20 @@
+﻿var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {
+            "url": "/Admin/User/GetAll"
+        },
+        "columns": [
+            { "data": "name", "width": "15%" },
+            { "data": "email", "width": "15%" },
+            { "data": "phoneNumber", "width": "15%" },
+            { "data": "company.name", "width": "15%" },
+            { "data": "role", "width": "15%" }
+        ]
+    });
+};
