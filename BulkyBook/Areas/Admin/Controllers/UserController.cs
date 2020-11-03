238f17ef767500acb8815f1aeda574489e686fa4
Jamie Everett <jreverett2442@gmail.com>
2020-11-03 16:10:19 +0000

    Add user lock/unlock functionality

--- BulkyBook/Areas/Admin/Controllers/UserController.cs
+++ BulkyBook/Areas/Admin/Controllers/UserController.cs
@@ -2,6 +2,7 @@
 using BulkyBook.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using System;
 using System.Linq;
 
 namespace BulkyBook.Areas.Admin.Controllers
@@ -44,6 +45,35 @@ namespace BulkyBook.Areas.Admin.Controllers
             }
 
             return Json(new { data = users });
+        }
+
+        [HttpPost]
+        public IActionResult ToggleLock([FromBody] string id)
+        {
+            var user = unitOfWork.ApplicationUser.GetFirstOrDefault(x => x.Id == id);
+            string response;
+
+            if (user == null)
+            {
+                return Json(new { success = false, message = "Error while locking/unlocking" });
+            }
+
+            if (user.LockoutEnd != null && user.LockoutEnd > DateTime.Now)
+            {
+                // Unlock
+                response = "unlocked";
+                user.LockoutEnd = DateTime.Now;
+            }
+            else
+            {
+                // Lock
+                response = "locked";
+                user.LockoutEnd = DateTime.Now.AddYears(1000);
+            }
+
+            unitOfWork.Save();
+
+            return Json(new { success = true, message = $"User {response} successfully" });
         }
 
         #endregion
--- BulkyBook/wwwroot/js/User.js
+++ BulkyBook/wwwroot/js/User.js
@@ -14,7 +14,54 @@ function loadDataTable() {
             { "data": "email", "width": "15%" },
             { "data": "phoneNumber", "width": "15%" },
             { "data": "company.name", "width": "15%" },
-            { "data": "role", "width": "15%" }
+            { "data": "role", "width": "15%" },
+            {
+                "data": { id: "id", lockoutEnd: "lockoutEnd" },
+                "render": function (data) {
+                    var today = new Date().getTime();
+                    var lockout = new Date(data.lockoutEnd).getTime();
+
+                    if (lockout > today) {
+                        // user is currently locked out
+                        return `
+                        <div class="text-center">
+                            <a class="btn btn-success text-white" onclick=ToggleLock("${data.id}") style="cursor: pointer; width: 100px">
+                                <i class="fas fa-lock-open"></i> Unlock
+                            </a>
+                        </div>
+                        `;
+                    }
+                    else {
+                        return `
+                        <div class="text-center">
+                            <a class="btn btn-danger text-white" onclick=ToggleLock("${data.id}") style="cursor: pointer; width: 100px">
+                                <i class="fas fa-lock"></i> Lock
+                            </a>
+                        </div>
+                        `;
+                    }
+
+                    
+                }, "width": "25%"
+            }
         ]
     });
 };
+
+function ToggleLock(id) {
+    $.ajax({
+        type: "POST",
+        url: "/Admin/User/ToggleLock",
+        data: JSON.stringify(id),
+        contentType: "application/json",
+        success: function (data) {
+            if (data.success) {
+                toastr.success(data.message);
+                dataTable.ajax.reload();
+            }
+            else {
+                toastr.error(data.message);
+            }
+        }
+    })
+}
